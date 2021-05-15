f526ebf152de61ad728575d802c9e54bf9de9d33
박상민 <iamsangminpark@gmail.com>
2021-05-15 11:43:18 +0900

    상민

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import MainHeader from './components/common/MainHeader';
 import Main from './pages/Main';
+<<<<<<< HEAD
 import First from './pages/First';
 import Signup from './pages/Signup';
 import SignupComplete from './pages/SignupComplete';
 import Test from './pages/Test';
+=======
+import ClosetMain from './pages/ClosetMain';
+import ClosetGrid from './pages/ClosetGrid';
+>>>>>>> cbdc9c8d392685ea23ff76928dc1fad992bd8de4
 
 function App() {
   return (
     <BrowserRouter>
+<<<<<<< HEAD
       <Route exact path="/" component={First} />
       <Route exact path="/main" component={Main} />
       <Route exact path="/signup" component={Signup} />
       <Route exact path="/signupcomplete" component={SignupComplete} />
       <Route exact path="/test" component={Test} />
+=======
+      <MainHeader />
+      <Route exact path="/" component={Main} />
+      <Route exact path="/closet" component={ClosetMain} />
+      <Route exact path="/closet/grid" component={ClosetGrid} />
+>>>>>>> cbdc9c8d392685ea23ff76928dc1fad992bd8de4
     </BrowserRouter>
   );
 }
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,13 +1,20 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+<<<<<<< HEAD
 import MainHeader from '../components/main/MainHeader';
 import MainContents from '../components/main/MainContents';
+=======
+>>>>>>> cbdc9c8d392685ea23ff76928dc1fad992bd8de4
 
 const Main = () => {
   return (
     <>
+<<<<<<< HEAD
       <MainHeader />
       <MainContents />
+=======
+      <h1>Main Page</h1>
+>>>>>>> cbdc9c8d392685ea23ff76928dc1fad992bd8de4
     </>
   );
 };
