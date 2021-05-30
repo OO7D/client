7f833c2b0748c5b7034099f1f8eef1393374b474
sooyeon oh <sooyeon0918@gmail.com>
2021-05-31 01:10:43 +0900

    test용 이미지

--- src/components/recResult/RatingBar.js
+++ src/components/recResult/RatingBar.js
@@ -4,17 +4,20 @@ import './RatingBar.css';
 
 const RatingBar = props => {
   const mode = useState(props.mode)[0];
+  let [_value, setValue] = useState(3);
   return (
     <>
       {/* SY: div id를 ratingBar로 설정하여 모달창에서 확인 버튼 누를 시 id로 찾아서 내용 변경하게 함 */}
       <div id='ratingBar'>
         <div className='ratingBar'>
           <span className='score' id='leftScore'>1점</span>
-          <input type="range" min="1" max="5" id="range" onChange={e => document.getElementById('rating').innerHTML = e.target.value + '점'}></input>
+          <input type="range" min="1" max="5" value={_value} id="range" onChange={e => {
+            setValue(e.target.value);
+            document.getElementById('rating').innerHTML = e.target.value + '점';}}></input>
           <div className='score' id='rightScore'>5점</div>
         </div>
         <div className='ratingBar'>
-          <div id='rating'>5점</div>
+          <div id='rating'>{_value}점</div>
         </div> 
       </div>
     </>
